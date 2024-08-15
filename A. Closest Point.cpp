1592c355df3d11e12c552b9281a1238e58b14729
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2024-08-16 00:08:09 +0600

    Update A. Closest Point.cpp

--- A. Closest Point.cpp
+++ A. Closest Point.cpp
@@ -8,52 +8,30 @@ int main()
     cin>>tt;
     while(tt--)
     {
-      long long n,k,i;
-      cin>>n>>k;
-      vector<long long>v;
-      for(i=1;i<=n;i++)
-      {
-          long long x;
-          cin>>x;
-          v.push_back(x);
-      }
-      sort(v.begin(),v.end());
-      reverse(v.begin(),v.end());
-      for(i=0;i<n;i++)
-      {
-          if(i%2==1)
-          {
-              long long sum=abs(v[i]-v[i-1]);
-              if(sum<=k)
-              {
-                  v[i]=v[i]+sum;
-                  k=k-sum;
-              }
-              else
-              {
-                  v[i]=v[i]+k;
-                  k=0;
-              }
-              if(k==0)
-              {
-                  break;
-              }
-          }
-      }
-      long long A=0,B=0;
-      for(i=0;i<n;i++)
-      {
-          if(i%2==0)
-          {
-              A+=v[i];
-          }
-          else
-          {
-              B+=v[i];
-          }
-      }
-      cout<<A-B<<endl;
+       long long n;
+       cin>>n;
+       long long arr[n+10],i;
+       for(i=1;i<=n;i++)
+       {
+           cin>>arr[i];
+       }
+       if(n==2)
+       {
+           long long diff=abs(arr[2]-arr[1]);
+           if(diff>=2)
+           {
+               cout<<"YES"<<endl;
+           }
+           else
+           {
+               cout<<"NO"<<endl;
+           }
 
+       }
+       else
+       {
+           cout<<"NO"<<endl;
+       }
     }
     return 0;
 }
